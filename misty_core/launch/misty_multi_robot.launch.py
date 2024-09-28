12b00f52cd814bf4bf364c8d59b02a5f5442564c
RodrigoCatto <rodrigo.catto7@gmail.com>
2024-09-28 10:55:43 -0400

    Added multi robot launch and namespacing

---
+++ misty_core/launch/misty_multi_robot.launch.py
@@ -0,0 +1,35 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='misty_core',
+            executable='misty_low_level',
+            name='misty_low_level_node',
+            namespace= "misty_3", 
+            output='screen',
+            emulate_tty=True,
+            # node_namespace='misty_3',
+            parameters=[
+                {'ip_address': '192.168.1.3'},
+                {'robot_name': 'misty_3'}
+            ]
+        ),
+        Node(
+            package='misty_core',
+            executable='misty_low_level',
+            name='misty_low_level_node',
+            namespace= "misty_2", 
+            output='screen',
+            emulate_tty=True,
+            # node_namespace='misty_3',
+            parameters=[
+                {'ip_address': '172.26.232.220'},
+                {'robot_name': 'misty_2'}
+            ]
+        ),
+    ])
+
+
+
