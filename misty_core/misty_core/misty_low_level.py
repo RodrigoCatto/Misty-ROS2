63a0e232a019f00e746e35ddda1de5d51bf1aefe
RodrigoCatto <rodrigo.catto7@gmail.com>
2024-09-28 10:02:53 -0400

    Add IP param change from lauch file

--- misty_core/misty_core/misty_low_level.py
+++ misty_core/misty_core/misty_low_level.py
@@ -1,7 +1,9 @@
 import rclpy
 from rclpy.node import Node
+from rcl_interfaces.msg import ParameterDescriptor
 
 from geometry_msgs.msg import Twist
+
 import mistyPy
 import time
 from numpy import clip
@@ -11,8 +13,15 @@ class MistyLowLevel(Node):
     def __init__(self):
         super().__init__('misty_low_level')
         print("Low level node created")
-        self.misty = mistyPy.Robot("192.168.1.3") #TODO make the IP as a parameter or class input
-        self.misty.stop() # Make sure that Misty is stopped
+
+        # Load parameters
+        my_parameter_descriptor = ParameterDescriptor(description='Misty IP Address')
+        self.declare_parameter('ip_address', '0.0.0.0', my_parameter_descriptor)
+        ip_adrress = self.get_parameter('ip_address').get_parameter_value().string_value
+        self.get_logger().info('IP: %s!' % ip_adrress)
+
+        self.misty = mistyPy.Robot(ip_adrress) #TODO error handling
+        self.misty.stop() # Make sure that Misty is halted
 
         self.vel_x = 0
         self.ang_z = 0
@@ -30,18 +39,19 @@ class MistyLowLevel(Node):
         self.set_misty_speed()
 
     def set_misty_speed(self):
-        #Convert m/s to misty -100 to 100 range
+        # Convert m/s to misty -100 to 100 range
         misty_vel_x = int(100*self.vel_x/self.misty_max_linear_speed)
         misty_angular_z = int(100*self.ang_z/self.misty_max_ang_speed)
         
+        # Clips the speeds to a max of 100 and a min of -100
         misty_vel_x = int(clip(misty_vel_x, -100, 100))
         misty_angular_z = int(clip(misty_angular_z, -100, 100))
 
-        if -10 > misty_vel_x < 10 and -10 > misty_angular_z < 10:
-            print("Commanding robot to stop")
+        if -10 > misty_vel_x < 10 and -10 > misty_angular_z < 10:  #TODO Check if enters this condition
+            self.get_logger().info("Halt")
             self.misty.stop()
         else:
-            print("Linear Speed: {}  Angular Speed {}".format(misty_vel_x, misty_angular_z))
+            self.get_logger().info("Linear Speed: {}  Angular Speed {}".format(misty_vel_x, misty_angular_z))
             self.misty.drive(misty_vel_x, misty_angular_z)
 
 
